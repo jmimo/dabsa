e95b51a8744a01c9055a3b2a350bfe97489fe142
Michael Mimo Moratti <mimo@mimo.ch>
2014-01-19 15:26:17 +0100

    dabs.py

--- dabs.py
+++ dabs.py
@@ -1,18 +1,11 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from sqlalchemy import create_engine, event
-from sqlalchemy.engine import Engine
-from sqlalchemy.orm import sessionmaker
+from flask.ext.googlemaps import GoogleMaps, Map
+import re
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-dbengine = create_engine('sqlite://///Users/mimo/Development/dabs/dabs.db')
-
-@event.listener_for(Engine, 'connect')
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute('PRAGMA foreign_keys=ON')
-    cursor.close()
+GoogleMaps(app)
 
 app.config.update(dict(
     DEBUG=True
@@ -31,17 +24,16 @@ def importAirspaces():
     if request.method == 'GET':
         return render_template('import.html', navloc='import')
 
+@app.route('/map')
+def map():
+    polygon = Map(identifier='polygon', lat=dms2dec(46,59,48), lng=dms2dec(8,22,58), style='height:450px;width:800px;margin:0;',
+            drawingtype='polygon', markers=[(dms2dec(46,57,13), dms2dec(8,27,52)), (dms2dec(46,57,46), dms2dec(8,30,41)),
+                (dms2dec(46,57,55), dms2dec(8,28,40)), (dms2dec(46,58,28), dms2dec(8,27,56)), (dms2dec(46,57,13), dms2dec(8,27,52))],
+    )
+    return render_template('map.html', navloc='map', gmap=polygon)
 
-def processAirspaceFile(airspaceFile):
-    Session = sessionmaker(bind=dbengine)
-    session = Session()
-    while True:
-        line = airpsaceFile.readline()
-        identifier = line[:2]
-        switch(identifier) {
-                case AC:
-        if not line:
-            break
+def dms2dec(degrees, minutes, seconds):
+    return int(degrees) + (float(minutes)/60) + (float(seconds)/3600)
 
 if __name__ == '__main__':
     app.run()
