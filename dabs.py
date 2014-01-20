03bb7b808098f16dd3968f761facf7a6ed8edd58
Michael Mimo Moratti <mimo@mimo.ch>
2014-01-20 19:06:07 +0100

    added new Polygon stuff

--- dabs.py
+++ dabs.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from flask.ext.googlemaps import GoogleMaps, Map
-import re
+from flask.ext.googlemaps import GoogleMaps, Map, Marker, Polygon
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -26,11 +25,29 @@ def importAirspaces():
 
 @app.route('/map')
 def map():
-    polygon = Map(identifier='polygon', lat=dms2dec(46,59,48), lng=dms2dec(8,22,58), style='height:450px;width:800px;margin:0;',
-            drawingtype='polygon', markers=[(dms2dec(46,57,13), dms2dec(8,27,52)), (dms2dec(46,57,46), dms2dec(8,30,41)),
-                (dms2dec(46,57,55), dms2dec(8,28,40)), (dms2dec(46,58,28), dms2dec(8,27,56)), (dms2dec(46,57,13), dms2dec(8,27,52))],
-    )
-    return render_template('map.html', navloc='map', gmap=polygon)
+    googlemap = Map(center=Marker(dms2dec(46,59,48), dms2dec(8,22,58)), style='height:450px;width:800px;margin:0;')
+
+    polygon1 = Polygon()
+    polygon1.add_marker(dms2dec(46,57,13), dms2dec(8,27,52))
+    polygon1.add_marker(dms2dec(46,57,46), dms2dec(8,30,41))
+    polygon1.add_marker(dms2dec(46,57,55), dms2dec(8,28,40))
+    polygon1.add_marker(dms2dec(46,58,28), dms2dec(8,27,56))
+    polygon1.add_marker(dms2dec(46,57,13), dms2dec(8,27,52))
+    googlemap.add_polygon(polygon1)
+    
+    polygon2 = Polygon()
+    polygon2.add_marker(dms2dec(46,56,23),dms2dec(8,23,38))
+    polygon2.add_marker(dms2dec(46,57,25),dms2dec(8,23,26))
+    polygon2.add_marker(dms2dec(46,57,21),dms2dec(8,22,30))
+    polygon2.add_marker(dms2dec(46,57,25),dms2dec(8,22,4))
+    polygon2.add_marker(dms2dec(46,57,41),dms2dec(8,21,52))
+    polygon2.add_marker(dms2dec(46,57,33),dms2dec(8,20,17))
+    polygon2.add_marker(dms2dec(46,57,2),dms2dec(8,19,57))
+    polygon2.add_marker(dms2dec(46,55,46),dms2dec(8,20,27))
+    polygon2.add_marker(dms2dec(46,56,23),dms2dec(8,23,38))
+    googlemap.add_polygon(polygon2)
+
+    return render_template('map.html', navloc='map', gmap=googlemap)
 
 def dms2dec(degrees, minutes, seconds):
     return int(degrees) + (float(minutes)/60) + (float(seconds)/3600)
