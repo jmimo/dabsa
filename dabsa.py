71e35883d360fa5d369b8facc0783a3c5eedc9e2
Michael Mimo Moratti <mimo@mimo.ch>
2014-01-21 18:41:27 +0100

    added file

---
+++ dabsa.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.googlemaps import GoogleMaps, Map, Marker, Polygon, Circle
+from flask.ext.sqlalchemy import SQLAlchemy
+
+import OpenAirParser
+from model import Area, Entry
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+GoogleMaps(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/mimo/Development/dabsa/test.db'
+db = SQLAlchemy(app)
+db.create_all()
+
+app.config.update(dict(
+    DEBUG=True
+))
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html', navloc='home')
+
+@app.route('/import', methods=['GET', 'POST'])
+def importAirspaces():
+    if request.method == 'POST':
+        file = request.files['airspace'] 
+
+        areas = OpenAirParser.parse(file)
+        for area in areas:
+            db.session.add(area)
+        db.session.commit()
+
+	return render_template('welcome.html', navloc='home')
+    if request.method == 'GET':
+        return render_template('import.html', navloc='import')
+
+@app.route('/map')
+def map():
+    googlemap = Map(center=Marker(dms2dec(46,59,48), dms2dec(8,22,58)), style='height:450px;width:800px;margin:0;')
+
+    googlemap.add_marker(dms2dec(46,59,48), dms2dec(8,22,58))
+
+    polygon1 = Polygon()
+    polygon1.add_marker(dms2dec(46,57,13), dms2dec(8,27,52))
+    polygon1.add_marker(dms2dec(46,57,46), dms2dec(8,30,41))
+    polygon1.add_marker(dms2dec(46,57,55), dms2dec(8,28,40))
+    polygon1.add_marker(dms2dec(46,58,28), dms2dec(8,27,56))
+    polygon1.add_marker(dms2dec(46,57,13), dms2dec(8,27,52))
+    googlemap.add_polygon(polygon1)
+    
+    polygon2 = Polygon()
+    polygon2.add_marker(dms2dec(46,56,23),dms2dec(8,23,38))
+    polygon2.add_marker(dms2dec(46,57,25),dms2dec(8,23,26))
+    polygon2.add_marker(dms2dec(46,57,21),dms2dec(8,22,30))
+    polygon2.add_marker(dms2dec(46,57,25),dms2dec(8,22,4))
+    polygon2.add_marker(dms2dec(46,57,41),dms2dec(8,21,52))
+    polygon2.add_marker(dms2dec(46,57,33),dms2dec(8,20,17))
+    polygon2.add_marker(dms2dec(46,57,2),dms2dec(8,19,57))
+    polygon2.add_marker(dms2dec(46,55,46),dms2dec(8,20,27))
+    polygon2.add_marker(dms2dec(46,56,23),dms2dec(8,23,38))
+    googlemap.add_polygon(polygon2)
+
+    circle = Circle(Marker(dms2dec(46,59,48), dms2dec(8,22,58)),100)
+    googlemap.add_circle(circle)
+
+    return render_template('map.html', navloc='map', gmap=googlemap)
+
+def dms2dec(degrees, minutes, seconds):
+    return int(degrees) + (float(minutes)/60) + (float(seconds)/3600)
+
+if __name__ == '__main__':
+    app.run()
