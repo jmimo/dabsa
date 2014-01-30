caa2a8eeae10b286f52054504580ffc199af8daa
Michael Mimo Moratti <michael.moratti@united-security-providers.ch>
2014-01-30 13:55:54 +0100

    added ajax roundtrip in dabsa.js file

--- views/m4p.py
+++ views/m4p.py
@@ -7,47 +7,12 @@ from model import Point
 import gis
 
 class MapView(BaseView):
-    methods = ['GET', 'POST']
+    methods = ['GET']
     
     def get_template_name(self):
         return 'map.html'
 
     def dispatch_request(self):
-        map_center = MAP_CENTER.split(":")
-        googlemap = Map(center=Marker(map_center[0],map_center[1]), cls="google-map", zoom=9)
-        model = self.get_objects()
-        if request.method == 'POST':
-            self.display_airspaces_within_selection(googlemap)
-
-        model['gmap'] = googlemap
-        return self.render_template(model)
-    
-    def display_airspaces_within_selection(self,googlemap):
-        amount = int(request.form['amount'])
-        points = []
-        formpoint = None
-        counter = 0
-        while counter < amount:
-            formpoint = request.form['point_' + str(counter)]
-            if not formpoint:
-                break
-            latlng = formpoint.split(":")
-            points.append((latlng[0],latlng[1]))
-            counter += 1
-        # TODO only load the relevant points based on the initial airspace type selection.
-        airspaces = gis.find_all_airspaces_inside_selected_polygon(points=Point.query.all(),polypoints=points)
-        self.construct_model(googlemap,airspaces)
-
-
-    def construct_model(self,googlemap,airspaces):
-        for airspace in airspaces:
-            marker = []
-            for point in airspace.points:
-                marker.append(Marker(latitude=point.latitude_dec,longitude=point.longitude_dec))
-            if len(marker) == 2:
-                googlemap.add_polyline(Polyline(airspace=airspace, markers=marker))
-            elif len(marker) > 2:
-                googlemap.add_polygon(Polygon(airspace=airspace, markers=marker))
-
-
+        return self.render_template(self.get_objects())
+   
 app.add_url_rule('/map', view_func=MapView.as_view('mapview'))
--- web/static/dabsa/dabsa.js
+++ web/static/dabsa/dabsa.js
@@ -2,10 +2,98 @@
 //
 // Author: Michael Mimo Moratti
 
-function initialize_google_map(htmlElementId, position, zoom) {
+// TODO: create shape parameter settings container
+
+function initialize_google_map(htmlElementId, position, mapType, zoom) {
   return new google.maps.Map(document.getElementById(htmlElementId), {
     center: position,
     zoom: zoom,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
+    mapTypeId: mapType
   });
+}
+
+function create_confirm_selection_dialog(processButtonFunctionName, removeButtonFunctionName) {
+ return new google.maps.InfoWindow({
+       content: '<div class="container-fluid"><b>Selection</b><br/><div class="btn-group btn-group-xs"><button class="btn btn-primary btn-success ladda-button" data-style="zoom-in" onclick="' + processButtonFunctionName  + '()"><span class="ladda-label">Submit</span><span class="ladda-spinner"></span></button><button class="btn btn-danger" onclick="' + removeButtonFunctionName + '()">Remove</button></div></div>'
+      });
+}
+
+function load_drawing_manager(map, confirmWindow, currentSelectionFunctionName) {
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+    },
+    polygonOptions: {
+      draggable: true,
+      editable: true,
+      strokeColor: '#FF6600',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FFCC00',
+      fillOpacity: 0.35
+    }  
+  });
+  drawingManager.setMap(map);
+
+  google.maps.event.addListener(drawingManager, 'polygoncomplete', function(polygon) {
+    google.maps.event.addListener(polygon, 'rightclick', function(event) {
+      window[currentSelectionFunctionName](polygon);
+      currentSelection = polygon;
+      confirmWindow.setPosition(event.latLng);
+      confirmWindow.open(map);
+      Ladda.bind('button');
+    });
+  });
+  }
+
+function fetch_selection_data(map, currentSelection, url) {
+  data = {};
+  currentSelection.getPath().forEach(function(latlng,i) {
+    data['point_' + i] = latlng.lat() + ':' + latlng.lng();
+  });
+  data['size'] = currentSelection.getPath().getLength();
+
+  $.post(url, data, function(response) {
+    confirmWindow.close();
+    removeSelection();
+    var airspaces = JSON.parse(response);
+    $.each(airspaces['airspaces'], function(key, value) {
+      var points = value['points'].length;
+      if(points == 2) {
+	 draw_polyline(map,value);
+      } else if(points > 2) {
+	 draw_polygon(map,value);
+      }
+    });
+  });
+}
+
+function draw_polygon(map, polygon) {
+  var polygon_def = new google.maps.Polygon({    
+    paths: create_coordinate_array(polygon),
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillcolor: '#FF0000',
+    fillopacity: 0.35
+  });
+  polygon_def.setMap(map);
+}
+
+function draw_polyline(map, polyline) {
+  var polyline_def = new google.maps.Polyline({
+    path: create_coordinate_array(polyline),
+    map: map 
+  });
+}
+
+function create_coordinate_array(drawingObject) {
+  var coords = [];
+  $.each(drawingObject['points'], function(index, point) {
+    coords[index] = new google.maps.LatLng(point['latitude_dec'],point['longitude_dec']);
+  });
+  return coords;
 }
