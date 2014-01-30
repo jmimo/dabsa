c4a4aa75fc22db7c873c74e92748cd236bc064ae
Michael Mimo Moratti <mimo@mimo.ch>
2014-01-30 08:12:47 +0100

    foo

--- views/ajax.py
+++ views/ajax.py
@@ -1,10 +1,25 @@
 from web import app
-from model import AirspaceFile
+from model import Point
 from flask import request
+import gis
 
-@app.route('/ajax/selection', methods=['GET','POST'])
+@app.route('/ajax/selection', methods=['POST'])
 def ajax_evaluate():
-    return request.form['polygon'] 
-    #afile = AirspaceFile.query.all()
-    #return str(afile[0].serialize)
+    size = int(request.form['size'])
+    points = []
+    formpoint = None
+    counter = 0
+    while counter < size:
+        formpoint = request.form['point_' + str(counter)]
+        if not formpoint:
+            break
+        latlng = formpoint.split(":")
+        points.append((latlng[0],latlng[1]))
+        counter += 1
 
+    airspaces = gis.find_all_airspaces_inside_selected_polygon(points=Point.query.all(),polypoints=points) 
+    response = '{['
+    for airspace in airspaces:
+        response += str(airspace.serialize)
+    response += ']}'
+    return response
