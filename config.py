a75de918a84fb0492608c01687e04d410cdd7552
Michael Mimo Moratti <mimo@mimo.ch>
2014-04-28 20:18:13 +0200

    added launch script dependent database configuration

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ MAP_CENTER='46.801111111111105:8.226666666666667'
 MAP_TYPE='google.maps.MapTypeId.SATELLITE'
 MAP_ZOOM=8
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dabsa.db')
-SQLALCHEMY_DATABASE_URI = 'mysql://dabsa:darkstar45@localhost/dabsa?charset=utf8&use_unicode=0'
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLITE_URI = 'sqlite:///' + os.path.join(basedir, 'dabsa.db')
+MYSQL_URI = 'mysql://dabsa:darkstar45@localhost/dabsa?charset=utf8&use_unicode=0'
+SQLALCHEMY_DATABASE_URI = MYSQL_URI
 
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import config
+
+config.SQLALCHEMY_DATABASE_URI = config.SQLITE_URI
+
 from web import app
 
 app.run(debug=True)
