fe1c4e5549f68e3c3a69571696e105ee3042f66e
Michael Mimo Moratti <mimo@mimo.ch>
2014-04-29 21:16:53 +0200

    added track structures

--- model.py
+++ model.py
@@ -91,3 +91,44 @@ class Point(Base):
             'latitude_dec': self.latitude_dec
         }
 
+
+
+class Track(Base):
+    __tablename__ = "Track"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    date = Column(DateTime)
+    points = relationship('TrackPoint', backref='track') 
+
+    @property
+    def serialize(self):
+        return {
+            'id': str(self.id),
+            'name': self.name,
+            'trackDate': (self.importDate.strftime("%Y-%n-%d %H:%M:%S") if self.importDate != None else None),
+            'trackpoints': [point.serialize for point in self.points]
+        }
+
+
+class TrackPoint(Base):
+    __tablename__ = "TrackPoint"
+    id = Column(Integer, primary_key=True)
+    index = Column(Integer)    
+    time = Column(DateTime)
+    longitude = Column(Float)
+    latitude = Column(Float)
+    altitude_ps = Column(Integer)
+    altitude_gps = Column(Integer)
+    track_id = Column(Integer, ForeignKey('Track.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'id': str(self.id),
+            'index': str(self.index),
+            'time': self.time.strftime("%H:%M:%S"),
+            'longitude': self.longitude,
+            'latitude': self.latitude,
+            'altitude_ps': str(self.altitude_ps),
+            'altitude_gps': str(self.altitude_gps)
+        }
