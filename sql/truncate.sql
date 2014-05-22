0a63bb718e0f7087cfbf0be044be3e75467ac608
Michael Mimo Moratti <mimo@mimo.ch>
2014-05-22 07:55:22 +0200

    added user table

--- sql/truncate.sql
+++ sql/truncate.sql
@@ -2,4 +2,6 @@ set foreign_key_checks=0;
 truncate table Point;
 truncate table Airspace;
 truncate table AirspaceFile;
+truncate table user;
 set foreign_key_checks=1;
+insert into user (username,password,email,registered_on) values ('mimo','darkstar45','mimo@mimo.ch',now());
